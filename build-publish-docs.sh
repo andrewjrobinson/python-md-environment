a057fd4c4749c8a48bbd2fcb801e8e455d418931
Andrew Robinson <andrew.robinson@latrobe.edu.au>
2016-04-13 11:24:44 +1000

    Deployed 2dc92d0 with MkDocs version: 0.15.3

--- build-publish-docs.sh
+++ build-publish-docs.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # this scripts builds the docs, places them into the gh-pages branch
-
+source "env/env-examples.sh"
 
 # so we get the current version and not the system installed one
 export PYTHONPATH="."
+
+export
 
 # do the deployment
 mkdocs gh-deploy

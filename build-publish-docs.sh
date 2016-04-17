d65ed7b4b8cf024c5da24424710d06564d1c26a0
Andrew Robinson <andrew.robinson@latrobe.edu.au>
2016-04-18 09:49:10 +1000

    Deployed ff6acba with MkDocs version: 0.15.3

--- build-publish-docs.sh
+++ build-publish-docs.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 # this scripts builds the docs, places them into the gh-pages branch
+
+# load the variables used in the docs
 source "env/env-examples.sh"
 
-# so we get the current version and not the system installed one
+# so we get the current version of mdx_environment and not the system installed one
 export PYTHONPATH="."
-
-export
 
 # do the deployment
 mkdocs gh-deploy
 
+# cleanup (causes issue with mkdocs if left behind)
+rm -R site

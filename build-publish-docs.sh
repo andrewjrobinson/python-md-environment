78effbb51ee2c2f1bf6489a42cb14bff807f36d9
Andrew Robinson <andrew.robinson@latrobe.edu.au>
2016-04-19 10:07:36 +1000

    Deployed c3fc072 with MkDocs version: 0.15.3

--- build-publish-docs.sh
+++ build-publish-docs.sh
@@ -3,13 +3,13 @@
 # this scripts builds the docs, places them into the gh-pages branch
 
 # load the variables used in the docs
-source "env/env-examples.sh"
+source "enviro/env-examples.sh"
 
 # so we get the current version of mdx_environment and not the system installed one
 export PYTHONPATH="."
 
 # do the deployment
-mkdocs gh-deploy
+mkdocs gh-deploy --clean
 
 # cleanup (causes issue with mkdocs if left behind)
 rm -R site
